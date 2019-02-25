8f61c8145ff84603268e5875243e891bedacacdd
Christopher <easonmu@hotmail.com>
2019-02-25 23:04:28 +0800

    router test

--- 前端技术/前端框架/react/react-project/src/App.js
+++ 前端技术/前端框架/react/react-project/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Redirect, Prompt} from "react-router-dom";
 // import logo from './logo.svg';
 import './App.css';
 
@@ -16,6 +16,12 @@ const BasicExample = () => (
         <li>
           <Link to="/topics">Topics</Link>
         </li>
+        {/*<li>
+          <Prompt message="Are you sure you want to leave?"/>
+        </li>*/}
+        {/*<li>
+          <Redirect to="/topics" />
+        </li>*/}
       </ul>
 
       <hr />
@@ -39,9 +45,35 @@ const About = () => (
   </div>
 );
 
-const Topics = ({ match }) => (
-  <div>
+// const Topics = ({ match, location }) => (
+//   <div>
+//     <h2>Topics</h2>
+//     <ul>
+//       <li>
+//         <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+//       </li>
+//       <li>
+//         <Link to={`${match.url}/components`}>Components</Link>
+//       </li>
+//       <li>
+//         <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+//       </li>
+//     </ul>
+
+//     <Route path={`${match.url}/:topicId`} component={Topic} />
+//     <Route
+//       exact
+//       path={match.url}
+//       render={() => <h3>Please select a topic.</h3>}
+//     />
+//   </div>
+// );
+
+const Topics = function({ match, location }){
+  console.log(location)
+  return <div>
     <h2>Topics</h2>
+    <div>{location.pathname}</div>
     <ul>
       <li>
         <Link to={`${match.url}/rendering`}>Rendering with React</Link>
@@ -61,7 +93,7 @@ const Topics = ({ match }) => (
       render={() => <h3>Please select a topic.</h3>}
     />
   </div>
-);
+};
 
 const Topic = ({ match }) => (
   <div>
